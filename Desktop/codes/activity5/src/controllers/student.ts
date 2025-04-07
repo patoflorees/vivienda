e8ddc7fe098cbbbd625460914bf7653f2dc87661
patoflorees <patoflores.garcia@icloud.com>
2025-04-07 12:21:28 -0600

    Delete Desktop/codes/activity5 directory

--- Desktop/codes/activity5/src/controllers/student.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import StudentHttpHandler from '../handlers/student';
-
-const router = express.Router();
-const studentHttpHandler = new StudentHttpHandler();
-
-router.get('/', studentHttpHandler.getStudents);
-
-export default router;
--- Desktop/codes/activity5/src/routes/student.ts
+++
@@ -1,9 +0,0 @@
-import getStudentsFromDB from '../DB/student';
-
-class StudentController {
-  async getAllStudents() {
-    return await getStudentsFromDB();
-  }
-}
-
-export default StudentController;
